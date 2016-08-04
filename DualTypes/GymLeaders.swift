e1590740f4a5ec292d799a447b27f949c22b8840
Robert Chen <robert.chen@thorntechnologies.com>
2016-08-03 23:04:53 -0400

    Creates 3 tabs

---
+++ DualTypes/GymLeaders.swift
@@ -0,0 +1,13 @@
+//
+//  GymLeaders.swift
+//  DualTypes
+//
+//  Created by Robert Chen on 8/3/16.
+//  Copyright © 2016 Robert Chen. All rights reserved.
+//
+
+import UIKit
+
+class GymLeaders: UICollectionViewController {
+    
+}
---
+++ DualTypes/Hexagon.swift
@@ -0,0 +1,13 @@
+//
+//  Hexagon.swift
+//  DualTypes
+//
+//  Created by Robert Chen on 8/3/16.
+//  Copyright © 2016 Robert Chen. All rights reserved.
+//
+
+import UIKit
+
+class Hexagon: UIViewController {
+    
+}
