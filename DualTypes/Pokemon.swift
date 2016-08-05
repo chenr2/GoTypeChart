c49a4f29642d78c938bb107c5f32d31ba2c22578
Robert Chen <robert.chen@thorntechnologies.com>
2016-08-04 22:20:01 -0400

    Data entry

--- DualTypes/Pokemon.swift
+++ DualTypes/Pokemon.swift
@@ -20,6 +20,88 @@ class Pokemon {
     class func vulnerabilitySet(element: ElementType) -> [ElementType: DamageType] {
         var returnSet = Pokemon.identityVulnerabilitySet()
         switch element {
+        case .Normal:
+            returnSet[.Fight] = .double
+            returnSet[.Ghost] = .none
+        case .Fight:
+            returnSet[.Flying] = .double
+            returnSet[.Rock] = .half
+            returnSet[.Bug] = .half
+            returnSet[.Psychic] = .double
+            returnSet[.Dark] = .half
+            returnSet[.Fairy] = .double
+        case .Flying:
+            returnSet[.Fight] = .half
+            returnSet[.Ground] = .none
+            returnSet[.Rock] = .double
+            returnSet[.Bug] = .half
+            returnSet[.Grass] = .half
+            returnSet[.Electric] = .double
+            returnSet[.Ice] = .double
+        case .Poison:
+            returnSet[.Fight] = .half
+            returnSet[.Poison] = .half
+            returnSet[.Ground] = .double
+            returnSet[.Bug] = .half
+            returnSet[.Grass] = .half
+            returnSet[.Psychic] = .double
+            returnSet[.Fairy] = .half
+        case .Ground:
+            returnSet[.Poison] = .half
+            returnSet[.Rock] = .half
+            returnSet[.Water] = .double
+            returnSet[.Grass] = .double
+            returnSet[.Electric] = .none
+            returnSet[.Ice] = .double
+        case .Rock:
+            returnSet[.Normal] = .half
+            returnSet[.Fight] = .double
+            returnSet[.Flying] = .half
+            returnSet[.Poison] = .half
+            returnSet[.Ground] = .double
+            returnSet[.Steel] = .double
+            returnSet[.Fire] = .half
+            returnSet[.Water] = .double
+            returnSet[.Grass] = .double
+        case .Bug:
+            returnSet[.Fight] = .half
+            returnSet[.Flying] = .double
+            returnSet[.Ground] = .half
+            returnSet[.Rock] = .double
+            returnSet[.Fire] = .double
+            returnSet[.Grass] = .half
+        case .Ghost:
+            returnSet[.Normal] = .none
+            returnSet[.Fight] = .none
+            returnSet[.Poison] = .half
+            returnSet[.Bug] = .half
+            returnSet[.Ghost] = .double
+            returnSet[.Dark] = .double
+        case .Steel:
+            returnSet[.Normal] = .half
+            returnSet[.Fight] = .double
+            returnSet[.Flying] = .half
+            returnSet[.Poison] = .none
+            returnSet[.Ground] = .double
+            returnSet[.Rock] = .half
+            returnSet[.Bug] = .half
+            returnSet[.Steel] = .half
+            returnSet[.Fire] = .double
+            returnSet[.Grass] = .half
+            returnSet[.Psychic] = .half
+            returnSet[.Ice] = .half
+            returnSet[.Dragon] = .half
+            returnSet[.Fairy] = .half
+        case .Fire:
+            returnSet[.Ground] = .double
+            returnSet[.Rock] = .double
+            returnSet[.Bug] = .half
+            returnSet[.Steel] = .half
+            returnSet[.Fire] = .half
+            returnSet[.Water] = .double
+            returnSet[.Grass] = .half
+            returnSet[.Ice] = .half
+            returnSet[.Fairy] = .half
         case .Water:
             returnSet[.Steel] = .half
             returnSet[.Fire] = .half
@@ -27,13 +109,48 @@ class Pokemon {
             returnSet[.Grass] = .double
             returnSet[.Electric] = .double
             returnSet[.Ice] = .half
+        case .Grass:
+            returnSet[.Flying] = .double
+            returnSet[.Poison] = .double
+            returnSet[.Ground] = .half
+            returnSet[.Bug] = .double
+            returnSet[.Fire] = .double
+            returnSet[.Water] = .half
+            returnSet[.Grass] = .half
+            returnSet[.Electric] = .half
+            returnSet[.Ice] = .double
+        case .Electric:
+            returnSet[.Flying] = .half
+            returnSet[.Ground] = .double
+            returnSet[.Steel] = .half
+            returnSet[.Electric] = .half
+        case .Psychic:
+            returnSet[.Fight] = .half
+            returnSet[.Bug] = .double
+            returnSet[.Ghost] = .double
+            returnSet[.Psychic] = .half
+            returnSet[.Dark] = .double
         case .Ice:
             returnSet[.Fight] = .double
             returnSet[.Rock] = .double
             returnSet[.Steel] = .double
             returnSet[.Fire] = .double
             returnSet[.Ice] = .half
-
+        case .Dragon:
+            returnSet[.Fire] = .half
+            returnSet[.Water] = .half
+            returnSet[.Grass] = .half
+            returnSet[.Electric] = .half
+            returnSet[.Ice] = .double
+            returnSet[.Dragon] = .double
+            returnSet[.Fairy] = .double
+        case .Fairy:
+            returnSet[.Fight] = .half
+            returnSet[.Poison] = .double
+            returnSet[.Bug] = .half
+            returnSet[.Steel] = .double
+            returnSet[.Dragon] = .none
+            returnSet[.Dark] = .half
         default:
             break
         }
