d642e66ecfad8b4b6eba320f6841c7d476d65a88
Robert Chen <robert.chen@thorntechnologies.com>
2016-08-04 20:58:54 -0400

    Getting some kind of info in there

--- DualTypes/GymLeaderDetail.swift
+++ DualTypes/GymLeaderDetail.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class GymLeaderDetail: UITableViewController {
     
-    var pokemon: Pokemon? = nil 
+    var pokemon: Pokemon? = nil {
+        didSet {
+            if let pokemon = pokemon {
+                quadruple = getKeysOfValue(.quadruple, pokemon: pokemon)
+                double = getKeysOfValue(.double, pokemon: pokemon)
+                half = getKeysOfValue(.half, pokemon: pokemon)
+                quarter = getKeysOfValue(.quarter, pokemon: pokemon)
+                none = getKeysOfValue(.none, pokemon: pokemon)
+            }
+        }
+    }
+    
+    var quadruple: [ElementType] = []
+    var double: [ElementType] = []
+    var half: [ElementType] = []
+    var quarter: [ElementType] = []
+    var none: [ElementType] = []
 
+    func getKeysOfValue(damage: DamageType, pokemon: Pokemon) -> [ElementType] {
+        var elements: [ElementType] = []
+        for (key, value) in pokemon.vulnerabilitySet {
+            if value == damage {
+                elements.append(key)
+            }
+        }
+        return elements
+    }
+    
 }
 
 extension GymLeaderDetail {
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
+        return 6
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -25,7 +51,15 @@ extension GymLeaderDetail {
         case 0:
             return 1
         case 1:
-            return 0
+            return quadruple.count
+        case 2:
+            return double.count
+        case 3:
+            return half.count
+        case 4:
+            return quarter.count
+        case 5:
+            return none.count
         default:
             return 0
         }
@@ -36,16 +70,54 @@ extension GymLeaderDetail {
 extension GymLeaderDetail {
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("GymLeaderDetailHeader") as! GymLeaderDetailHeader
-        cell.textLabel?.text = pokemon?.name
-        let elementArray = pokemon?.defense.map {
-            return $0.rawValue
+
+        let detailTypeCell = tableView.dequeueReusableCellWithIdentifier("GymLeaderDetailTypeCell") as! GymLeaderDetailTypeCell
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCellWithIdentifier("GymLeaderDetailHeader") as! GymLeaderDetailHeader
+            cell.textLabel?.text = pokemon?.name
+            let elementArray = pokemon?.defense.map {
+                return $0.rawValue
+            }
+            let elementString = elementArray?.joinWithSeparator(" / ")
+            if let elementString = elementString {
+                cell.detailTextLabel?.text = "Type:  \(elementString)"
+            }
+            return cell
+        case 1:
+            detailTypeCell.textLabel?.text = quadruple[indexPath.row].rawValue
+        case 2:
+            detailTypeCell.textLabel?.text = double[indexPath.row].rawValue
+        case 3:
+            detailTypeCell.textLabel?.text = half[indexPath.row].rawValue
+        case 4:
+            detailTypeCell.textLabel?.text = quarter[indexPath.row].rawValue
+        case 5:
+            detailTypeCell.textLabel?.text = none[indexPath.row].rawValue
+        default:
+            break
         }
-        let elementString = elementArray?.joinWithSeparator(" / ")
-        if let elementString = elementString {
-            cell.detailTextLabel?.text = "Type:  \(elementString)"            
+        return detailTypeCell
+
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return ""
+        case 1:
+            return "Elements that do 4x damage:"
+        case 2:
+            return "Elements that do 2x damage:"
+        case 3:
+            return "Elements that do 1/2x damage:"
+        case 4:
+            return "Elements that do 1/4x damage:"
+        case 5:
+            return "Elements that do no damage:"
+        default:
+            return ""
         }
-        return cell
     }
 
 }
--- DualTypes/Pokemon.swift
+++ DualTypes/Pokemon.swift
@@ -8,9 +8,91 @@
 
 import Foundation
 
+enum DamageType {
+    case none, single, double, quadruple, half, quarter
+}
+
 class Pokemon {
     var name: String
     var defense: [ElementType]
+    var vulnerabilitySet: [ElementType: DamageType]
+    
+    class func vulnerabilitySet(element: ElementType) -> [ElementType: DamageType] {
+        var returnSet = Pokemon.identityVulnerabilitySet()
+        switch element {
+        case .Water:
+            returnSet[.Steel] = .half
+            returnSet[.Fire] = .half
+            returnSet[.Water] = .half
+            returnSet[.Grass] = .double
+            returnSet[.Electric] = .double
+            returnSet[.Ice] = .half
+        default:
+            break
+        }
+        return returnSet
+    }
+    
+    class func identityVulnerabilitySet() -> [ElementType: DamageType] {
+        return [
+            .Normal: .single,
+            .Fight: .single,
+            .Flying: .single,
+            .Poison: .single,
+            .Ground: .single,
+            .Rock: .single,
+            .Bug: .single,
+            .Ghost: .single,
+            .Steel: .single,
+            .Fire: .single,
+            .Water: .single,
+            .Grass: .single,
+            .Electric: .single,
+            .Psychic: .single,
+            .Ice: .single,
+            .Dragon: .single,
+            .Dark: .single,
+            .Fairy: .single
+        ]
+    }
+    
+    class func multiplyDamageTypes(a:DamageType?, b:DamageType?) -> DamageType {
+        guard let a = a else { return b! }
+        guard let b = b else { return a }
+        // multiply by zero
+        if a == .none || b == .none {
+            return .none
+        }
+        if a == .single {
+            return b
+        }
+        // multiply by one
+        if b == .single {
+            return a
+        }
+        // multiply halves or double
+        switch (a, b) {
+        case (.half, .double), (.double, .half):
+            return .single
+        case (.half, .half):
+            return .quarter
+        case (.double, .double):
+            return .quadruple
+        default:
+            print("This should not happen")
+            return .single
+        }
+    }
+    
+    class func multiplyVulnerabilitySets(setA: [ElementType: DamageType], setB: [ElementType: DamageType]) -> [ElementType: DamageType] {
+        var productVulnerabilities = Pokemon.identityVulnerabilitySet()
+        let keys = Array(setA.keys) + Array(setB.keys)
+        let uniqueKeys = Array(Set(keys))
+        for key in uniqueKeys {
+            productVulnerabilities[key] = multiplyDamageTypes(setA[key]!, b: setB[key]!)
+        }
+        return productVulnerabilities
+    }
     
     class func gymLeaders() -> [Pokemon] {
         let vaporeon = Pokemon(name: "Vaporeon", defense: [.Water])
@@ -22,5 +104,13 @@ class Pokemon {
     init(name: String, defense: [ElementType]){
         self.name = name
         self.defense = defense
+        let vulnerabilitySets = defense.map {
+            return Pokemon.vulnerabilitySet($0)
+        }
+        if defense.count == 1 {
+            self.vulnerabilitySet = vulnerabilitySets.first!
+        } else {
+            self.vulnerabilitySet = Pokemon.multiplyVulnerabilitySets(vulnerabilitySets.first!, setB: vulnerabilitySets.last!)
+        }
     }
 }
