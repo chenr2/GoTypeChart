a6ebff3d7cf3765c74140c4eab19073ef420ebd6
Robert Chen <robert.chen@thorntechnologies.com>
2016-08-15 22:15:12 -0400

    Cleaning up the grid cells

--- DualTypes/GymLeaderCell.swift
+++ DualTypes/GymLeaderCell.swift
@@ -11,29 +11,12 @@ import UIKit
 class GymLeaderCell: UICollectionViewCell {
     
     @IBOutlet var pokemonName: UILabel!
-    @IBOutlet weak var pokemonType: UILabel!
-    @IBOutlet weak var pokedexNumber: UILabel!
     @IBOutlet weak var sortLabel: UILabel!
     
     var pokemon: Pokemon? = nil
     
     func configureCell(pokemon: Pokemon, sortType: SortType){
         self.pokemon = pokemon
-        pokedexNumber?.text = "#\(pokemon.pokedex)"
         pokemonName?.text = pokemon.name
-        let elementArray = pokemon.type.map {
-            return $0.rawValue
-        }
-        pokemonType?.text = elementArray.joinWithSeparator(" / ")
-        switch sortType {
-        case .attack:
-            sortLabel.text = "A: \(pokemon.attack)"
-        case .defense:
-            sortLabel.text = "D: \(pokemon.defense)"
-        case .stamina:
-            sortLabel.text = "S: \(pokemon.stamina)"
-        default:
-            sortLabel.text = ""
-        }
     }
 }
--- DualTypes/StatCell.swift
+++ DualTypes/StatCell.swift
@@ -11,31 +11,23 @@ import UIKit
 class StatCell: UICollectionViewCell {
     
     @IBOutlet var pokemonName: UILabel!
-    @IBOutlet weak var pokemonType: UILabel!
-    @IBOutlet weak var pokedexNumber: UILabel!
     @IBOutlet weak var sortLabel: UILabel!
-    @IBOutlet weak var statIcon: UIImageView!
     
     var pokemon: Pokemon? = nil
     
     func configureCell(pokemon: Pokemon, sortType: SortType){
         self.pokemon = pokemon
-        pokedexNumber?.text = "#\(pokemon.pokedex)"
         pokemonName?.text = pokemon.name
         let elementArray = pokemon.type.map {
             return $0.rawValue
         }
-        pokemonType?.text = elementArray.joinWithSeparator(" / ")
         switch sortType {
         case .attack:
             sortLabel.text = "\(pokemon.attack)"
-            statIcon.image = UIImage.init(named: "punch")
         case .defense:
             sortLabel.text = "\(pokemon.defense)"
-            statIcon.image = UIImage.init(named: "shield")
         case .stamina:
             sortLabel.text = "\(pokemon.stamina)"
-            statIcon.image = UIImage.init(named: "energy")
         default:
             sortLabel.text = ""
         }
