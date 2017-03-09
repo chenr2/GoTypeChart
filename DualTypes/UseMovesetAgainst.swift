2a5e1437a6305610f6da9e4f4c4155ed8fb36335
Robert Chen <robert.chen@thorntechnologies.com>
2017-03-08 22:08:55 -0500

    Adds the dodging segmented control to modal

--- DualTypes/UseMovesetAgainst.swift
+++ DualTypes/UseMovesetAgainst.swift
@@ -10,8 +10,24 @@ import UIKit
 
 class UseMovesetAgainst: UITableViewController {
     
+    var dodging: Dodge = .none
+
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true){}
+    }
+    
+    @IBAction func toggleDodging(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            dodging = .none
+        case 1:
+            dodging = .charge
+        case 2:
+            dodging = .both
+        default:
+            break
+        }
+        recalculateBestOptions()
     }
     
     var quickAttack: QuickAttack? = nil
@@ -20,17 +36,26 @@ class UseMovesetAgainst: UITableViewController {
 
     var bestOptions: [AverageMon] = []
 
-    override func viewDidLoad() {
+    func recalculateBestOptions(){
         if let pokemon = pokemon,
             let quickAttack = quickAttack,
             let specialAttack = specialAttack {
-            bestOptions = Pokemon.calculatePotentialTargetsFor(
-                pokemon,
-                quickAttack: quickAttack,
-                specialAttack: specialAttack,
-                dodging: .both
-            )
-            tableView.reloadData()
+                bestOptions = Pokemon.calculatePotentialTargetsFor(
+                    pokemon,
+                    quickAttack: quickAttack,
+                    specialAttack: specialAttack,
+                    dodging: dodging
+                )
+                tableView.reloadData()
+        }
+
+    }
+
+    override func viewDidLoad() {
+        recalculateBestOptions()
+        if let pokemon = pokemon,
+            let quickAttack = quickAttack,
+            let specialAttack = specialAttack {
             if let titleControl = Bundle.main.loadNibNamed("DetailTitleControl", owner: nil, options: nil)![0] as? DetailTitleControl {
                 titleControl.configureTitleControl(
                     pokemon,
@@ -43,23 +68,36 @@ class UseMovesetAgainst: UITableViewController {
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return bestOptions.count
+        if section == 0 {
+            return 1
+        } else {
+            return bestOptions.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
-        let option = bestOptions[(indexPath as NSIndexPath).row]
-        cell.textLabel?.text = NSLocalizedString(option.opponent.species.rawValue, comment: "")
-        cell.detailTextLabel?.text = String(format: "%.1f", (-1 * option.average))
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UseMovesetAgainstDodgeCell") as! UseMovesetAgainstDodgeCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+            let option = bestOptions[(indexPath as NSIndexPath).row]
+            cell.textLabel?.text = NSLocalizedString(option.opponent.species.rawValue, comment: "")
+            cell.detailTextLabel?.text = String(format: "%.1f", (-1 * option.average))
+            return cell
+        }
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Average advantage against defender"
+        if section == 1 {
+            return "Average advantage against defender"
+        } else {
+            return "Dodging"
+        }
     }
     
 }
---
+++ DualTypes/UseMovesetAgainstDodgeCell.swift
@@ -0,0 +1,13 @@
+//
+//  UseMovesetAgainstDodgeCell.swift
+//  DualTypes
+//
+//  Created by Robert Chen on 3/8/17.
+//  Copyright © 2017 Robert Chen. All rights reserved.
+//
+
+import UIKit
+
+class UseMovesetAgainstDodgeCell: UITableViewCell {
+    
+}
